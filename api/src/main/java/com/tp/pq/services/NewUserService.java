d78daebc3a5264c3e3049fdec50ef852f9205adb
Matthew Tramantano <matt.tramantano@gmail.com>
2023-05-14 04:30:04 -0600

    added createNewUser function

--- api/src/main/java/com/tp/pq/services/NewUserService.java
+++ api/src/main/java/com/tp/pq/services/NewUserService.java
@@ -1,11 +1,14 @@
 package com.tp.pq.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.tp.pq.models.Rule;
 import com.tp.pq.models.Ruling;
 import com.tp.pq.models.Schedule;
+import com.tp.pq.models.TimeBlock;
 import com.tp.pq.models.User;
 import com.tp.pq.repos.RuleRepo;
 import com.tp.pq.repos.RulingRepo;
@@ -29,10 +32,23 @@ public class NewUserService {
 
     public void createNewUser(String username, String password) {
         User user = userRepo.save(User.builder().username(username).password(password).build());
-        // Rules
-        // Rule rule1 = ruleRepo.save(Rule.builder().description("Rule
-        // 1").ruling(ruling).build());
-        // Ruling ruling = rulingRepo.save(Ruling.builder().name("Ruling
-        // 1").description("Ruling 1 description").color("red").user(user).build());
+
+        Rule defualtRule = Rule.builder().description("My very first rule to follow!").build();
+        Ruling defaultRuling = Ruling.builder().name("My first ruling").description("My first ruling description")
+                .color("red").user(user).build();
+        defaultRuling.addRule(defualtRule);
+        user.addRuling(defaultRuling);
+
+        Schedule defaultSchedule = Schedule.builder().name("My first schedule")
+                .user(user).build();
+        List<TimeBlock> defaultTimeBlocks = defaultSchedule.createDefaultSchedule();
+        user.addSchedule(defaultSchedule);
+
+        userRepo.save(user);
+        scheduleRepo.save(defaultSchedule);
+        rulingRepo.save(defaultRuling);
+        ruleRepo.save(defualtRule);
+        timeBlockRepo.saveAll(defaultTimeBlocks);
+
     }
 }
